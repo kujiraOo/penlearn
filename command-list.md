6d988a01accfabd7a94c1a931e6a8258e032b5bf
Ivan Penguin <screemers@mail.ru>
2021-01-25 00:56:14 +0300

    open new branch

---
+++ command-list.md
@@ -0,0 +1,3 @@
+git status // show changed files
+git diff // show difference with  last commit (head) in current branch
+git add [some_file_name] // aadd fille (or all if use '.') to stage changes
--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
-const emptyObject = {};
+const emptyObject = {};// прото пустой объект без всего
 
 const notEmptyObject = {
-  a: 4, // assigning a value
+  a: 4, // assigning a value (property of the object)
   b: 5,
   c: 'some string',
-};
+};// object with 2 numbers and 1 string
 
-const nestedObject = {
+const nestedObject = { // object with object in it and number and string in object in object
   someProp: {
     c: 'ok',
   },
@@ -18,30 +18,31 @@ const nestedObject = {
 };
 
 // Accessing properties of an object
-const something = notEmptyObject.c; // 'some string', accessing a value
-const somethingNested = nestedObject.anotherProp.moreNestedProperty; // {}, accessing a nested property
+const something = notEmptyObject.c; // 'some string', accessing a value. something get 'some string' value, also it is variables and get his own house in memory
+const somethingNested = nestedObject.anotherProp.moreNestedProperty; // {}, accessing a nested property. same with previous but get (access) some nested property
 
-const { someProp } = nestedObject;
+const { someProp } = nestedObject; // if you want to create variables with same name as property of already created object you can use this way
 const { anotherProp } = nestedObject;
 
 const veryAnotherProp = nestedObject.veryAnotherProp; // same as const { veryAnotherProp } = nestedObject;
 
 // Assigning values to properties of an object
-notEmptyObject.c = 'another value'; // re-assigning value of the property
+notEmptyObject.c = 'another value'; // re-assigning value of the property. change value of c of notEmptyObject to another value
 
-const amazingValue = 10;
+const amazingValue = 10; // new variables
 const anotherAmazingValue = 20;
 
 const amazingObject = {
-  amazingValue,
-  b: anotherAmazingValue,
-  someObjectHere: { a: 3 },
-};
+  amazingValue, // look here bitch, its simple way
+  b: anotherAmazingValue, // look here bitch its not simple way
+  someObjectHere: { a: 3 }, // its just an object
+  d: 5
+}; // create object with 10 , 20 , and object with 3
 
 const objectWithSpread = {
   something: 3,
   ...amazingObject, // spreading an object
-};
+}; // spreading take all properties of an object one by one and copy them in another object
 
 const shallowCopyOfAmazingObject = {
   ...amazingObject,
@@ -52,8 +53,15 @@ const copyOfAmazingObject = {
   someObjectHere: { ...amazingObject.someObjectHere },
 }; // different address in memory
 
-const anotherObjectHere = amazingObject.someObjectHere;
-const copyOfSomeObjectHere = { ...anotherObjectHere };
+const bitch = amazingObject
+const {someObjectHere} = amazingObject
+someObjectHere.a = 4;
+amazingObject.d = 6;
+
+//console.log(amazingObject, shallowCopyOfAmazingObject, copyOfAmazingObject,someObjectHere,bitch);
+
+const anotherObjectHere = amazingObject.someObjectHere; // same with 57 line
+const copyOfSomeObjectHere = { ...anotherObjectHere }; // spreading like shallowCopy
 
 shallowCopyOfAmazingObject.someObjectHere.a = 4;
 
@@ -67,17 +75,16 @@ copyOfSomeObjectHere.a = 6;
 // console.log('copyOfAmazingObject.someObjectHere', copyOfAmazingObject.someObjectHere);
 // console.log('copyOfSomeObjectHere', copyOfSomeObjectHere);
 
-const weirdPropertyName = 'some space here';
+const weirdPropertyName = 'some space here'; // new variable with string
 
 const objectWithWeirdPropertyNames = {
-  [weirdPropertyName]: 'ok',
+  [weirdPropertyName]: 'ok', // create KEY with same name as value of variables
   [4]: 'ofrfr',
+  ['ok'] : 'not ok'
 };
-
-console.log(objectWithWeirdPropertyNames);
-console.log(objectWithWeirdPropertyNames[weirdPropertyName]);
-
 // COMMON OBJECT FUNCTIONS / KEYWORDS
+
+//console.log(objectWithWeirdPropertyNames);
 
 const objectWithPropertyToDelete = { propertyToDelete: 'delete me' };
 
@@ -92,16 +99,18 @@ objectWithPropertyToDelete.propertyToNotDelete2 = 'don\'t delete';
 objectWithPropertyToDelete.propertyToNotDelete3 = 'don\'t delete';
 
 const objectWithKeys = {
-  a: 3,
+  a : [1,2,3,4],
   b: 4,
   nyaaa: 5,
   nyaaa2: 'some string',
 };
 
-const keysOfObjectWithKeys = Object.keys(objectWithKeys);
+const keysOfObjectWithKeys = Object.keys(objectWithKeys); //call to "white" keys
 
-console.log(keysOfObjectWithKeys);
+const valuesOfObjectWithKeys = Object.values(objectWithKeys); //call to values of properties
 
-const valuesOfObjectWithKeys = Object.values(objectWithKeys);
-
-console.log(valuesOfObjectWithKeys);
+objectWithKeys.a[10] = 'anything';
+objectWithKeys.a[6] = undefined;
+console.log(objectWithKeys,keysOfObjectWithKeys,valuesOfObjectWithKeys);
+console.log(objectWithKeys.a[6])
+console.log(objectWithKeys.a.length);
