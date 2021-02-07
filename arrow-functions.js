13adad03ecb19d907a3277b4cb0963a9b7af330c
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-02-07 22:40:39 +0200

    add array functions and arrow functions examples

---
+++ arrow-functions.js
@@ -0,0 +1,13 @@
+// Coming next
+
+const arrowFunctionWithArgumentAndReturnValue = (x) => x;
+
+const arrowFunctionWithBody = () => {};
+
+const arrowFunctionWithArgumentBodyAndReturnValue = (x) => {
+  const double = 2 * x;
+
+  return double - 100;
+};
+
+const multiply = (x, y) => x * y;
