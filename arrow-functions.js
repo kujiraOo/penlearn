5057f90611bd2b01d46797a18c0b2e6fa5ae8b63
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-02-14 15:50:42 +0200

    fix lint in arrow-functions.js

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,13 +1,17 @@
 // Coming next
 
+// eslint-disable-next-line no-unused-vars
 const arrowFunctionWithArgumentAndReturnValue = (x) => x;
 
+// eslint-disable-next-line no-unused-vars
 const arrowFunctionWithBody = () => {};
 
+// eslint-disable-next-line no-unused-vars
 const arrowFunctionWithArgumentBodyAndReturnValue = (x) => {
   const double = 2 * x;
 
   return double - 100;
 };
 
+// eslint-disable-next-line no-unused-vars
 const multiply = (x, y) => x * y;
