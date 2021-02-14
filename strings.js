b7d02006e681333060e863eea28de7e089bd59cd
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-02-14 15:16:44 +0200

    fix lint for string.js

--- strings.js
+++ strings.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const someStringToInterpolate = 'nyaaa';
 
 console.log(`This is a very long sentence, but here i want to use a value of a variable ${someStringToInterpolate} And here the snetence continues withj fdsaome speliing aerors and`);
