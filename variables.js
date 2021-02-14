d2fe1faa780ca80132df3065bf8c719f280b94b5
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-02-14 15:15:54 +0200

    fix lint in variables.js

--- variables.js
+++ variables.js
@@ -1,5 +1,8 @@
+// eslint-disable-next-line no-unused-vars, no-var
 var a = 3; // never use it
 
+// eslint-disable-next-line no-unused-vars, prefer-const
 let b = 3; // use this if you cannot use const
 
+// eslint-disable-next-line no-unused-vars
 const c = 3; // always use this
