dbcc0d7a408ea567d7cd5ec8056c527bca561a8f
Ivan Penguin <screemers@mail.ru>
2021-04-17 01:06:27 +0300

    following plan

--- battle/round-system.js
+++ battle/round-system.js
@@ -3,19 +3,135 @@ const idGenerator = require('./id-generator');
 
 const nextId = idGenerator();
 
-const alliedParty = newParty.createAllies(nextId);
+// const alliedParty = newParty.createAllies(nextId);
 
-const enemyParty = newParty.createEnemies(nextId);
+// const enemyParty = newParty.createEnemies(nextId);
 
-const turnCreate = (team1) => team1.map(({ id, name, movePoints }) => ({ id, name, movePoints }));
+const alliedParty = [{
+  id: 1,
+  name: 'Penguin',
+  hp: 33,
+  mp: 0,
+  agl: 17,
+  dmg: 7,
+  def: 3,
+  movePoints: 17,
+},
+{
+  id: 2,
+  name: 'Sawa',
+  hp: 28,
+  mp: 16,
+  agl: 21,
+  dmg: 9,
+  def: 0,
+  movePoints: 21,
+}];
 
-const turnLog = [...turnCreate(alliedParty), ...turnCreate(enemyParty)];
+const enemyParty = [{
+  id: 3,
+  name: 'Golem',
+  hp: 35,
+  mp: 0,
+  agl: 8,
+  dmg: 3,
+  def: -3,
+  movePoints: 8,
+},
+{
+  id: 4,
+  name: 'Golem Lord',
+  hp: 70,
+  mp: 1,
+  agl: 10,
+  dmg: 6,
+  def: -6,
+  movePoints: 10,
+}];
 
-const pickUnitWithMaxMovepoints = (turnLogToSort) => turnLogToSort.sort(
+const unitToTurnLogItem = ({
+  id, name, agl, movePoints,
+}) => ({
+  id, name, agl, movePoints,
+});
+
+const copyUnit = (unit) => ({ ...unit });
+
+/* const turnCreate = (team1) => team1.map(unitToTurnLogItem);
+
+const turnCreate2 = (team) => {
+  const team2 = [];
+  for (let i = 0; i < team.length; i += 1) {
+    team2.push(unitToTurnLogItem(team[i]));
+  }
+  return team2;
+}; */
+
+const turnLog = [...alliedParty.map(copyUnit), ...enemyParty.map(copyUnit)];
+
+const sortUnitByMovepoints = (turnLogToSort) => turnLogToSort.sort(
   (movePoints1, movePoints2) => movePoints2.movePoints - movePoints1.movePoints,
 );
 
-console.log(pickUnitWithMaxMovepoints(turnLog));
+const pickUnitWithMaxMovepoints = (units) => {
+  sortUnitByMovepoints(units);
+  return units[0];
+};
+
+const moveCost = 6;
+
+const calculateMovepoints = (units, movingUnitId) => {
+  const newUnitsArray = units.map(copyUnit);
+  const unit = newUnitsArray.find((element) => element.id === movingUnitId);
+  unit.movePoints -= moveCost;
+  return newUnitsArray;
+};
+
+const restoreMovepoints = (units) => {
+  const newUnitsArray = units.map(copyUnit);
+  newUnitsArray.forEach((element) => {
+    element.movePoints += element.agl;
+  });
+  return newUnitsArray;
+};
+
+const unitsHaveEnoughMovepoints = (units) => !!units.find((unit) => unit.movePoints >= moveCost);
+
+const calculateTurn = (units) => {
+  console.log(units.map(unitToTurnLogItem));
+  const unit = pickUnitWithMaxMovepoints(units);
+  console.log(unit);
+  /* if (!unitHasEnoughMovepoints(unit)) {
+    const restoredUnits = restoreMovepoints(units);
+    const pickedRestoredUnit = pickUnitWithMaxMovepoints(restoredUnits);
+    calculateMovepoints(restoredUnits, pickedRestoredUnit.id);
+    return pickedRestoredUnit;
+  }
+  */
+  calculateMovepoints(units, unit.id);
+  return unit;
+};
+
+const turnQueue = (units) => {
+  const queue = [];
+  let tempUnits = units;
+  for (let i = 0; i < 12; i += 1) {
+    if (!unitsHaveEnoughMovepoints(tempUnits)) {
+      tempUnits = restoreMovepoints(tempUnits);
+    }
+    const unit = pickUnitWithMaxMovepoints(tempUnits);
+    queue.push(unit);
+    tempUnits = calculateMovepoints(tempUnits, unit.id);
+  }
+  return queue;
+};
+// console.log(sortUnitByMovepoints(turnLog).map(unitToTurnLogItem));
+
+// console.log(calculateMovepoints(turnLog, 2).map(unitToTurnLogItem));
+
+console.log(turnQueue(turnLog).map(unitToTurnLogItem));
+
+// console.log(restoreMovepoints(turnLog));
 
 /* console.log(turnLog(alliedParty));
 
