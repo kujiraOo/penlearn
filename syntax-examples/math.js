c6c1d43aa1c9a70768e335f8932c3fdabeff55f7
Ivan Penguin <screemers@mail.ru>
2021-04-18 23:43:32 +0300

    add divide

--- syntax-examples/math.js
+++ syntax-examples/math.js
@@ -4,8 +4,16 @@ const add = (a, b) => a + b;
 
 const multiply = (a, b) => a * b;
 
+const divide = (a, b) => {
+  if (b === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return a / b;
+};
+
 module.exports = {
   PI,
   add,
   multiply,
+  divide,
 };
