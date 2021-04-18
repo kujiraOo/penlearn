641f5e7c5cbae0bca7bc3358d428f713ad7e2415
Ivan Penguin <screemers@mail.ru>
2021-04-18 22:16:40 +0300

    all done

--- battle/round-system.js
+++ battle/round-system.js
@@ -1,6 +1,6 @@
 const moveCost = 6;
 
-const logLength = 12;
+const turnQueueLength = 12;
 
 const alliedParty = [{
   id: 1,
@@ -50,41 +50,40 @@ const unitToTurnLogItem = ({
   id, name, agl, movePoints,
 });
 
-const copyObject = (unit) => ({ ...unit });
+const copyObject = (object) => ({ ...object });
 
 const turnLog = [...alliedParty.map(copyObject), ...enemyParty.map(copyObject)];
 
-const sortUnitsByMovepoints = (units) => units.sort(
-  (unit1, unit2) => unit2.movePoints - unit1.movePoints,
-);
+const sortUnitsByMovePoints = (units) => [...units]
+  .sort((unit1, unit2) => unit2.movePoints - unit1.movePoints);
 
-const pickUnitWithMaxMovepoints = (units) => {
-  sortUnitsByMovepoints(units);
+const pickUnitWithMaxMovePoints = (units) => {
+  sortUnitsByMovePoints(units);
   return units[0];
 };
 
-const calculateMovepoints = (units, movingUnitId) => {
+const calculateMovePoints = (units, movingUnitId) => {
   const newUnitArray = units.map(copyObject);
   const unit = newUnitArray.find((element) => element.id === movingUnitId);
   unit.movePoints -= moveCost;
   return newUnitArray;
 };
 
-const restoreMovepoints = (units) => units
+const restoreMovePoints = (units) => units
   .map((unit) => ({ ...unit, movePoints: unit.movePoints + unit.agl }));
 
-const unitsHaveEnoughMovepoints = (units) => !!units.find((unit) => unit.movePoints >= moveCost);
+const unitsHaveEnoughMovePoints = (units) => !!units.find((unit) => unit.movePoints >= moveCost);
 
 const turnQueue = (units) => {
   const queue = [];
   let tempUnits = units;
-  for (let i = 0; i < logLength; i += 1) {
-    if (!unitsHaveEnoughMovepoints(tempUnits)) {
-      tempUnits = restoreMovepoints(tempUnits);
+  for (let i = 0; i < turnQueueLength; i += 1) {
+    if (!unitsHaveEnoughMovePoints(tempUnits)) {
+      tempUnits = restoreMovePoints(tempUnits);
     }
-    const unit = pickUnitWithMaxMovepoints(tempUnits);
+    const unit = pickUnitWithMaxMovePoints(tempUnits);
     queue.push(unit);
-    tempUnits = calculateMovepoints(tempUnits, unit.id);
+    tempUnits = calculateMovePoints(tempUnits, unit.id);
   }
   return queue;
 };
