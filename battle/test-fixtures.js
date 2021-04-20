bd99ef46b78a5ef0dc1da810b5468d5f81e100cc
PenguinSatan <53793464+PenguinSatan@users.noreply.github.com>
2021-04-21 00:04:57 +0300

    Ai (#29)
    Add selectAttackTarget and attack

---
+++ battle/test-fixtures.js
@@ -0,0 +1,44 @@
+const units = [{
+  id: 1,
+  name: 'Penguin',
+  hp: 120,
+  def: 12,
+  attack: 21,
+  agl: 17,
+  movePoints: 17,
+  partyId: 'Allies',
+},
+{
+  id: 2,
+  name: 'Sawa',
+  hp: 100,
+  def: 6,
+  attack: -12,
+  agl: 21,
+  movePoints: 21,
+  partyId: 'Allies',
+},
+{
+  id: 3,
+  name: 'Sadomasochist Golem',
+  hp: 35,
+  def: -3,
+  attack: 12,
+  agl: 8,
+  movePoints: 8,
+  partyId: 'Foes',
+},
+{
+  id: 4,
+  name: 'Golem Lord',
+  hp: 50,
+  def: 3,
+  attack: 18,
+  agl: 10,
+  movePoints: 10,
+  partyId: 'Foes',
+}];
+
+module.exports = {
+  units,
+};
