fb02a687e7d0287ffb81b5c54a06be3617bbea78
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-04-28 22:20:37 +0300

    remove syntax-examples

--- syntax-examples/strings.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-console */
-const someStringToInterpolate = 'nyaaa';
-
-console.log(`This is a very long sentence, but here i want to use a value of a variable ${someStringToInterpolate} And here the snetence continues withj fdsaome speliing aerors and`);
--- syntax-examples/variables.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars, no-var
-var a = 3; // never use it
-
-// eslint-disable-next-line no-unused-vars, prefer-const
-let b = 3; // use this if you cannot use const
-
-// eslint-disable-next-line no-unused-vars
-const c = 3; // always use this
