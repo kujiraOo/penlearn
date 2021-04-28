09bb0707f6bf4deeb04544c57c45a9e530e29d8d
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-04-28 23:12:24 +0300

    install koa router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
+
+const examples = require('./controllers/examples');
 
 const port = 3000;
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'BATTLE!11';
-});
+const router = new Router({ prefix: '/api' });
+
+router.use(
+  examples.routes(),
+  examples.allowedMethods(),
+);
+
+app.use(
+  router.routes(),
+  router.allowedMethods(),
+);
 
 app.listen(port, () => {
   console.log(`battle listens at port ${port}`);
