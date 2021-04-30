4eb9b15f6c7b3b13ac90cc2b38021f8ceacd8b71
Ivan Penguin <screemers@mail.ru>
2021-04-30 22:39:59 +0300

    wip: advanced ai

--- src/ai.js
+++ src/ai.js
@@ -1,10 +1,55 @@
-const selectAttackTarget = (unit, units) => {
-  const sortedUnitsArray = units
-    .filter((u) => u.partyId !== unit.partyId)
-    .sort((unit1, unit2) => unit1.hp - unit2.hp);
-  return sortedUnitsArray[0];
+const { turnQueue } = require('./round-system');
+
+const findEnemies = (actor, units) => units
+  .filter((u) => u.partyId !== actor.partyId);
+
+const findAlly = (actor, units) => units
+  .find((unit) => unit.partyId === actor.partyId && unit.id !== actor.id);
+
+const selectTargetWithLowestHp = (enemies) => {
+  const targets = enemies.sort((unit1, unit2) => unit1.hp - unit2.hp);
+
+  return targets[0];
+};
+
+const killableEnemies = (actor, enemies) => enemies
+  .filter((e) => e.hp < actor.attack);
+
+const enemiesActBeforeTarget = (ally, units) => {
+  const queue = turnQueue(units);
+  const allyTurn = queue.find((u) => u.id === ally.id);
+  const enemiesActingBeforeTarget = queue.filter((e) => e.partyId !== ally.partyId)
+    .filter((actingEnemies) => actingEnemies.movePoints > allyTurn.movePoints);
+  return enemiesActingBeforeTarget;
+};
+
+const enemiesThatCanKillAlly = (ally, enemiesActingBeforeTarget) => enemiesActingBeforeTarget
+  .filter((e) => e.attack > ally.hp);
+
+const targetEnemyBeforeAllyDeath = (actor, units) => {
+  const ally = findAlly(actor, units);
+  const targets = enemiesThatCanKillAlly(ally, enemiesActBeforeTarget(ally, units));
+  const target = killableEnemies(actor, targets).sort((e1, e2) => e1.movePoints - e2.movePoints);
+
+  return target[0];
+};
+
+const selectAttackTarget = (actor, units) => {
+  const enemies = findEnemies(actor, units);
+  const target = selectTargetWithLowestHp(enemies);
+
+  return target;
 };
 
 module.exports = {
   selectAttackTarget,
+  killableEnemies,
+  findEnemies,
+  targetEnemyBeforeAllyDeath,
+  findAlly,
 };
+
+/* const target = units
+    .filter((u) => u.partyId !== actor.partyId)
+    .sort((unit1, unit2) => unit1.hp - unit2.hp);
+ */
--- src/ai.test.js
+++ src/ai.test.js
@@ -1,10 +1,149 @@
 const { units } = require('./test-fixtures');
-const { selectAttackTarget } = require('./ai');
+const {
+  selectAttackTarget,
+  killableEnemies,
+  findEnemies,
+  findAlly,
+} = require('./ai');
 
 describe('selectAttackTarget', () => {
   test('returns unit with lowest hp from opposite party', () => {
     const sawa = units[1];
     const sadomasochistGolem = selectAttackTarget(sawa, units);
     expect(sadomasochistGolem.id).toBe(3);
+  });
+});
+
+describe('selectAttackTarget', () => {
+  test('returns killable units from opposite party', () => {
+    const allies = [{
+      id: 1,
+      name: 'Penguin',
+      hp: 1,
+      def: 0,
+      attack: 21,
+      agl: 12,
+      movePoints: 12,
+      partyId: 'Allies',
+    },
+    {
+      id: 2,
+      name: 'Sawa',
+      hp: 100,
+      def: 6,
+      attack: 9999,
+      agl: 21,
+      movePoints: 21,
+      partyId: 'Allies',
+    },
+    ];
+    const foes = [{
+      id: 3,
+      name: 'Sadomasochist Golem',
+      hp: 35,
+      def: -3,
+      attack: 12,
+      agl: 8,
+      movePoints: 8,
+      partyId: 'Foes',
+    },
+    {
+      id: 4,
+      name: 'Golem Lord',
+      hp: 50,
+      def: 3,
+      attack: 18,
+      agl: 14,
+      movePoints: 14,
+      partyId: 'Foes',
+    }];
+    const testUnits = [...allies, ...foes];
+
+    const sawa = allies[1];
+
+    const killableUnits = killableEnemies(sawa, findEnemies(sawa, testUnits));
+
+    expect(killableUnits).toMatchObject([{
+      id: 3,
+      name: 'Sadomasochist Golem',
+      hp: 35,
+      def: -3,
+      attack: 12,
+      agl: 8,
+      movePoints: 8,
+      partyId: 'Foes',
+    },
+    {
+      id: 4,
+      name: 'Golem Lord',
+      hp: 50,
+      def: 3,
+      attack: 18,
+      agl: 14,
+      movePoints: 14,
+      partyId: 'Foes',
+    }]);
+  });
+
+  test('returns correct ally', () => {
+    const allies = [{
+      id: 1,
+      name: 'Penguin',
+      hp: 1,
+      def: 0,
+      attack: 21,
+      agl: 12,
+      movePoints: 12,
+      partyId: 'Allies',
+    },
+    {
+      id: 2,
+      name: 'Sawa',
+      hp: 100,
+      def: 6,
+      attack: 9999,
+      agl: 21,
+      movePoints: 21,
+      partyId: 'Allies',
+    },
+    ];
+    const foes = [{
+      id: 3,
+      name: 'Sadomasochist Golem',
+      hp: 35,
+      def: -3,
+      attack: 12,
+      agl: 8,
+      movePoints: 8,
+      partyId: 'Foes',
+    },
+    {
+      id: 4,
+      name: 'Golem Lord',
+      hp: 50,
+      def: 3,
+      attack: 18,
+      agl: 14,
+      movePoints: 14,
+      partyId: 'Foes',
+    }];
+    const testUnits = [...allies, ...foes];
+
+    const sawa = allies[1];
+
+    const ally = findAlly(sawa, testUnits);
+
+    expect(ally).toMatchObject(
+      {
+        id: 1,
+        name: 'Penguin',
+        hp: 1,
+        def: 0,
+        attack: 21,
+        agl: 12,
+        movePoints: 12,
+        partyId: 'Allies',
+      },
+    );
   });
 });
