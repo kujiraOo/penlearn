109d82f34bf5f3e5494e78a88cd83afaddd57262
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-05-02 17:22:28 +0300

    install pg, add random-numbers controllers

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('@koa/router');
 
-const examples = require('./controllers/examples');
+const dbPool = require('./db/db-pool');
+const dbPoolMiddleware = require('./middleware/db-pool-middleware');
+const examples = require('./controllers/random-numbers');
 
 const port = 3000;
 const app = new Koa();
-
 const router = new Router({ prefix: '/api' });
 
 router.use(
   examples.routes(),
   examples.allowedMethods(),
 );
+
+app.use(dbPoolMiddleware(dbPool()));
 
 app.use(bodyParser());
 
