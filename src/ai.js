907470160ac25001507e3d95f574f895e8d80ee9
Penguin <screemers@mail.ru>
2021-05-15 01:02:08 +0300

    fix selectKillableEnemy sort

--- src/ai.js
+++ src/ai.js
@@ -31,7 +31,7 @@ const selectPreemptibleKiller = (actor, units) => {
     .filter(isEnemyOf(ally))
     .filter(canKill(ally))
     .filter(canBeKilledBy(actor))
-    .sort(byMovePointsDesc)[0];
+    .sort(byHpDesc)[0];
 };
 
 const selectAttackTarget = (actor, units) => {
