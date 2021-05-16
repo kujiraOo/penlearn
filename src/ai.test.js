8af01e4ca21194d70cc8477924513e2f26b43882
Penguin <screemers@mail.ru>
2021-05-16 21:55:05 +0300

    add test on sortByHp

--- src/ai.test.js
+++ src/ai.test.js
@@ -196,5 +196,76 @@ describe('ai', () => {
         },
       );
     });
+
+    test('returns killable enemy that can kill ally and have highest hp', () => {
+      const units = [
+        {
+          id: 2,
+          name: 'Sawa',
+          hp: 100,
+          def: 6,
+          attack: 9999,
+          agl: 21,
+          movePoints: 16,
+          partyId: 'Allies',
+        },
+        {
+          id: 4,
+          name: 'Golem Lord',
+          hp: 35,
+          def: 3,
+          attack: 18,
+          agl: 14,
+          movePoints: 14,
+          partyId: 'Foes',
+        },
+        {
+          id: 3,
+          name: 'Sadomasochist Golem',
+          hp: 50,
+          def: -3,
+          attack: 12,
+          agl: 13,
+          movePoints: 13,
+          partyId: 'Foes',
+        },
+        {
+          id: 1,
+          name: 'Penguin',
+          hp: 1,
+          def: 0,
+          attack: 21,
+          agl: 12,
+          movePoints: 12,
+          partyId: 'Allies',
+        },
+        {
+          id: 4,
+          name: 'Lord of Golem Lord',
+          hp: 99,
+          def: 3,
+          attack: 18,
+          agl: 8,
+          movePoints: 8,
+          partyId: 'Foes',
+        },
+      ];
+      const actor = units[0];
+
+      const target = selectAttackTarget(actor, units);
+
+      expect(target).toMatchObject(
+        {
+          id: 3,
+          name: 'Sadomasochist Golem',
+          hp: 50,
+          def: -3,
+          attack: 12,
+          agl: 13,
+          movePoints: 13,
+          partyId: 'Foes',
+        },
+      );
+    });
   });
 });
