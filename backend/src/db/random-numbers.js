f715b1c851784ddfe37c074657f20fc1e9722b47
Penguin <screemers@mail.ru>
2021-06-01 23:57:50 +0300

    add get controller

--- backend/src/db/random-numbers.js
+++ backend/src/db/random-numbers.js
@@ -12,14 +12,26 @@ module.exports = {
 
   selectAll: sql`select id, value from random_numbers`,
 
+  select: (id) => ({
+    text: sql`
+      select id, min, max, value, created_at, updated_at
+      from random_numbers
+      where id = $1
+    `,
+
+    values: [id],
+
+  }),
+
   update: ({
     min, max, value, id,
   }) => ({
     text: sql`
-    update random_numbers 
-      set min = $1, max = $2, value = $3, updated_at = now() 
-    where id = $4
-    returning min, max, value, id, created_at, updated_at`,
+      update random_numbers
+      set min = $1, max = $2, value = $3 
+      where id = $4
+      returning min, max, value, id, created_at, updated_at
+    `,
 
     values: [min, max, value, id],
 
