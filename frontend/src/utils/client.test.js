b9d83fe76d1b80e992aa17296185077110d6c285
Arsenii Kurilov <arsenii.kurilov@gmail.com>
2021-06-05 23:23:40 +0300

    Add getRandomNumbers tests

--- frontend/src/utils/client.test.js
+++ frontend/src/utils/client.test.js
@@ -1,18 +1,53 @@
 import { getRandomNumbers } from './client';
 
 describe('client', () => {
-  beforeEach(() => {
-    global.fetch = jest.fn().mockReturnValue({});
+  beforeAll(() => {
+    global.fetch = jest.fn();
   });
 
   describe('getRandomNumbers', () => {
-    it('should call fetch with correct url', async () => {
+    const successResponse = [
+      { id: 1, value: 60 },
+      { id: 2, value: 70 },
+      { id: 3, value: 80 },
+    ];
+
+    const successResolvedValue = {
+      ok: true,
+      json: async () => successResponse,
+    };
+
+    const setUpMockFetch = (resolvedValue) => global.fetch.mockResolvedValue(resolvedValue);
+
+    it('calls fetch with correct parameters', async () => {
+      setUpMockFetch(successResolvedValue);
+
       await getRandomNumbers();
 
       expect(global.fetch).toHaveBeenCalledWith(
         '/api/random-numbers',
         { method: 'GET', headers: { 'Content-Type': 'application/json' } },
       );
+    });
+
+    it('returns list of random number entries when request succeeds', async () => {
+      setUpMockFetch(successResolvedValue);
+
+      const result = await getRandomNumbers();
+
+      expect(result).toMatchObject(successResponse);
+    });
+
+    it('returns null when request fails', async () => {
+      setUpMockFetch({
+        ok: false,
+        status: 500,
+        statusText: 'Mock fail error',
+      });
+
+      const result = await getRandomNumbers();
+
+      expect(result).toBe(null);
     });
   });
 });
